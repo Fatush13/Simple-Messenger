e4d5a0644bbf7c4ca56f66c0f1620aea26efba34
Fatush13 <fatush13@gmail.com>
2020-02-22 22:11:09 +0200

    Added few unit tests

---
+++ src/test/java/com/example/pmster/UserServiceTest.java
@@ -0,0 +1,106 @@
+package com.example.pmster;
+
+import com.example.pmster.domain.Role;
+import com.example.pmster.domain.User;
+import com.example.pmster.repos.UserRepo;
+import com.example.pmster.service.MailSender;
+import com.example.pmster.service.UserService;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collections;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @MockBean
+    private UserRepo userRepo;
+
+    @MockBean
+    private MailSender mailSender;
+
+    @MockBean
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    public void addUser() {
+        User user = new User();
+
+        user.setEmail("email@mail.ru");
+
+        boolean isUserCreated = userService.addUser(user);
+
+        Assert.assertTrue(isUserCreated);
+        Assert.assertNotNull(user.getActivationCode());
+        Assert.assertTrue(CoreMatchers.is(user.getRoles()).matches(Collections.singleton(Role.USER)));
+
+        Mockito.verify(userRepo, Mockito.times(1)).save(user);
+        Mockito.verify(mailSender, Mockito.times(1))
+                .send(
+                        ArgumentMatchers.eq(user.getEmail()),
+                        ArgumentMatchers.eq("Activation code"),
+                        ArgumentMatchers.contains("Welcome to PMster!"));
+    }
+
+    @Test
+    public void addUserFailTest() {
+        User user = new User();
+
+        user.setUsername("User");
+
+        Mockito.doReturn(new User())
+                .when(userRepo)
+                .findByUsername("User");
+
+        boolean isUserCreated = userService.addUser(user);
+
+        Assert.assertFalse(isUserCreated);
+
+        Mockito.verify(userRepo, Mockito.times(0)).save(ArgumentMatchers.any(User.class));
+        Mockito.verify(mailSender, Mockito.times(0))
+                .send(
+                        ArgumentMatchers.anyString(),
+                        ArgumentMatchers.anyString(),
+                        ArgumentMatchers.anyString());
+    }
+
+    @Test
+    public void activateUser() {
+        User user = new User();
+
+        user.setActivationCode("Acode");
+
+        Mockito.doReturn(user)
+                .when(userRepo)
+                .findByActivationCode("activate");
+
+        boolean isUserActivated = userService.activateUser("activate");
+
+        Assert.assertTrue(isUserActivated);
+        Assert.assertNull(user.getActivationCode());
+
+        Mockito.verify(userRepo, Mockito.times(1)).save(user);
+    }
+
+    @Test
+    public void activateUserFailTest() {
+        boolean isUserActivated = userService.activateUser(("activate user"));
+
+        Assert.assertFalse(isUserActivated);
+
+        Mockito.verify(userRepo, Mockito.times(0)).save(ArgumentMatchers.any(User.class));
+    }
+}
